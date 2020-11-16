2df5e41187a1eb1cd539848a7e48b998bd9fd5f9
Alex <aalexribeiro@gmail.com>
2020-11-16 20:27:46 -0300

    Criação do DTO

--- src/main/java/com/alex/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/alex/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.alex.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.alex.cursomc.domain.Categoria;
+import com.alex.cursomc.dto.CategoriaDTO;
 import com.alex.cursomc.services.CategoriaService;
 
 @RestController
@@ -26,6 +29,13 @@ public class CategoriaResource {
 		Categoria categoria = catService.find(id);
 		
 		return ResponseEntity.ok(categoria);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = catService.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok(listDto);
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
